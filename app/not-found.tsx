5384ddc7e094d1e30b5dcc1f55d9c8684235edb3
Nikunj Chauhan <nkca122@gmail.com>
2026-02-04 11:27:25 +0000

    init 4

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+"use client"
+export default function(){
+    return (
+        <>
+            <div className="top-0 left-0 absolute w-screen h-screen flex justify-center items-center">
+                <h1 className="text-4xl font-bungee font-extrabold">
+                    <span className="text-red-500">404</span> | Not Found
+                </h1>
+            </div>
+        </>
+    )
+}
