208606b390087748fd06466c823f257b4feb9b53
Nikunj Chauhan <nkca122@gmail.com>
2026-02-04 10:41:12 +0000

    init 2

--- lib/events/actions.ts
+++ lib/events/actions.ts
@@ -2,7 +2,7 @@ import { unstable_cache } from "next/cache";
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 export const getEvents = unstable_cache(
   async () => {
-    const response = await fetch(`${BASE_URL}/api/events`);
+    const response = await fetch(`/api/events`);
     const data = await response.json();
     if (data?.success) {
       return data?.events;
